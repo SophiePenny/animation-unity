09ccb5a05a16c7abccf679d28b6f44ae63d876ed
SophiePenny <72386509+SophiePenny@users.noreply.github.com>
2020-11-02 16:19:02 +0000

    Initial commit

---
+++ Assets/Scripts/animation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("walkleft", true);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("walkright", true);
+        }
+
+        if(Input.GetKey("d"))
+        {
+            anim.SetBool("walkright2", true);
+        }
+
+        if(Input.GetKey("a"))
+        {
+            anim.SetBool("walkleft2", true);
+        }
+    }
+}
