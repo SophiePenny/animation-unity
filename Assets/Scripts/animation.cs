f09aee738e480bc6deb4dc00931a9d76b3a7179b
SophiePenny <72386509+SophiePenny@users.noreply.github.com>
2020-11-04 14:01:53 +0000

    animation works correctly

--- Assets/Scripts/animation.cs
+++ Assets/Scripts/animation.cs
@@ -13,25 +13,46 @@ public class animation : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {  
+        anim.SetBool("stand", true);
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             anim.SetBool("walkleft", true);
+            anim.SetBool("stand", false);
+        }
+        else
+        {
+            anim.SetBool("walkleft", false);
         }
 
         if(Input.GetKey(KeyCode.RightArrow))
         {
             anim.SetBool("walkright", true);
+            anim.SetBool("stand", false);
+        }
+        else
+        {
+            anim.SetBool("walkright", false);
         }
 
         if(Input.GetKey("d"))
         {
             anim.SetBool("walkright2", true);
+            anim.SetBool("stand", false);
+        }
+        else
+        {
+            anim.SetBool("walkright2", false);
         }
 
         if(Input.GetKey("a"))
         {
             anim.SetBool("walkleft2", true);
+            anim.SetBool("stand", false);
+        }
+        else
+        {
+            anim.SetBool("walkleft2", false);
         }
     }
 }
